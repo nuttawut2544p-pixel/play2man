969cf14ccbbdab1bba0fd69a979c919da922bb84
nuttawut2544p-pixel <nuttawut2544p@gmail.com>
2026-06-29 00:33:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+console.log("Play2Man Website Loaded 🚀");
 
+const btn = document.querySelector(".join-btn");
+
+btn.addEventListener("mouseenter",()=>{
+    btn.style.transform="scale(1.08)";
+});
+
+btn.addEventListener("mouseleave",()=>{
+    btn.style.transform="scale(1)";
+});
