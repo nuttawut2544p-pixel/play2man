6395c359fef1b146fb616de3ac909267832367bf
nuttawut2544p-pixel <nuttawut2544p@gmail.com>
2026-06-29 00:41:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,75 @@
-console.log("Play2Man Website Loaded 🚀");
+// Scroll Animation
 
-const btn = document.querySelector(".join-btn");
+const sections=document.querySelectorAll("section");
 
-btn.addEventListener("mouseenter",()=>{
-    btn.style.transform="scale(1.08)";
+const observer=new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
 });
 
-btn.addEventListener("mouseleave",()=>{
-    btn.style.transform="scale(1)";
 });
+
+sections.forEach(sec=>{
+
+sec.classList.add("hidden");
+
+observer.observe(sec);
+
+});
+
+// Back To Top
+
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=()=>{
+
+if(window.scrollY>400){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+}
+
+topBtn.onclick=()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
+
+// Floating Particles
+
+const container=document.getElementById("particles");
+
+for(let i=0;i<70;i++){
+
+const p=document.createElement("div");
+
+p.className="particle";
+
+p.style.left=Math.random()*100+"%";
+
+p.style.animationDuration=(5+Math.random()*10)+"s";
+
+p.style.animationDelay=Math.random()*5+"s";
+
+container.appendChild(p);
+
+}
